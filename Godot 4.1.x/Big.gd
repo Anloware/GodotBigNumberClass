3342bfe0914e779088efb9f2c59d77fb53218c06
Andri Wagner <86930766+andriwagner@users.noreply.github.com>
2025-04-03 13:37:09 +0200

    Adjust toPrefix() to remove decimals with values of zero if force decimals equals false

--- Godot 4.1.x/Big.gd
+++ Godot 4.1.x/Big.gd
@@ -1,3 +1,26 @@
+# MIT License
+# 
+# Copyright (c) 2020 ChronoDK
+# Copyright (c) 2025 Anloware Ltd
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 class_name Big
 extends RefCounted
 ## Big number class for use in idle / incremental games and other games that needs very large numbers
@@ -734,7 +757,7 @@ func toPrefix(no_decimals_on_small_values = false, use_thousand_symbol=true, for
 		for _i in range(exponent % 3):
 			hundreds *= 10
 		number *= hundreds
-
+	
 	var split := str(number).split(".")
 	if split.size() == 1:
 		split.append("")
@@ -743,6 +766,8 @@ func toPrefix(no_decimals_on_small_values = false, use_thousand_symbol=true, for
 		for i in range(max_decimals):
 			if split[1].length() < max_decimals:
 				split[1] += "0"
+	elif int(split[1]) == 0:
+		split[1] = ""
 	
 	if no_decimals_on_small_values and exponent < 3:
 		return split[0]
